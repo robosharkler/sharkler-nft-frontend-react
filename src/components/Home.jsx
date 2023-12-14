98f649b8b0f0519291cf3e6dfd647be33aa5ab13
SallYBot <mr.sharkyn@gmail.com>
2023-12-13 22:15:47 -0800

    be able to load frontend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import WalletBalance from './WalletBalance';
-import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
 import SharklerNft from '../../sharkler-nft-contract/src/artifacts/contracts/SharklerNft.sol/SharklerNft.json';
 
 const contractAddress = '0xF1f3E5e56A8A562cF0010dEb06484AbfBEc43353';
@@ -9,7 +9,6 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 // get the smart contract
 const contract = new ethers.Contract(contractAddress, SharklerNft.abi, signer);
-
 
 function Home() {
   const [totalMinted, setTotalMinted] = useState(0);
@@ -32,9 +31,59 @@ function Home() {
         .fill(0)
         .map((_, i) => (
             <div key={i}>
-              1
+              <NFTImage tokenId={i} getCount={getCount} />
             </div>
         ))}
+    </div>
+  );
+}
+
+function NFTImage({ tokenId, getCount }) {
+  const metadataCID = 'QmR9nXy6XHmJcGLXHDjWN79BhaQEN21LU4kU4V2QvkdbDN';
+  const metadataURI = `ipfs://${metadataCID}/${tokenId}.json`;
+  const imageCID = 'QmesATCiAPVNA6X7C3TnbMnvZjK4g27HAV3cdr1aTKYeH4'
+  const imageURI = `https://ipfs.io/ipfs/${imageCID}/${tokenId}.png`;
+
+  const [isMinted, setIsMinted] = useState(false);
+  useEffect(() => {
+    getMintedStatus();
+  }, [isMinted]);
+
+  const getMintedStatus = async () => {
+    const result = await contract.isContentOwned(metadataURI);
+    console.log(result)
+    setIsMinted(result);
+  };
+
+  const mintToken = async () => {
+    const connection = contract.connect(signer);
+    const addr = connection.address;
+    const result = await contract.payToMint(addr, metadataURI, {
+      value: ethers.utils.parseEther('0.05'),
+    });
+
+    await result.wait();
+    getMintedStatus();
+    getCount();
+  };
+
+  async function getURI() {
+    const uri = await contract.tokenURI(tokenId);
+    alert(uri);
+  }
+  return (
+    <div>
+      <img src={isMinted ? imageURI : "src/assets/placeholder.png"} width="240" height="240"></img>
+        <h5>ID #{tokenId}</h5>
+        {!isMinted ? (
+          <button onClick={mintToken}>
+            Mint
+          </button>
+        ) : (
+          <button onClick={getURI}>
+            Taken! Show URI
+          </button>
+        )}
     </div>
   );
 }
