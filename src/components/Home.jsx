922aa0926e2fc59dcd26416882760e0d1417cc76
SallYBot <mr.sharkyn@gmail.com>
2023-12-13 23:16:49 -0800

    do not show invalid NFT created in testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const contract = new ethers.Contract(contractAddress, SharklerNft.abi, signer);
 
 function Home() {
   const [totalMinted, setTotalMinted] = useState(0);
-  
+
   // update totalMinted
   const getCount = async () => {
     const count = await contract.count();
@@ -26,13 +26,13 @@ function Home() {
 
   return (
     <div>
-        <WalletBalance />
-        {Array(totalMinted + 1)
+      <WalletBalance />
+      {Array(totalMinted + 1) 
         .fill(0)
         .map((_, i) => (
-            <div key={i}>
-              <NFTImage tokenId={i} getCount={getCount} />
-            </div>
+          <div key={i}>
+            <NFTImage tokenId={i} getCount={getCount} />
+          </div>
         ))}
     </div>
   );
@@ -41,8 +41,28 @@ function Home() {
 function NFTImage({ tokenId, getCount }) {
   const metadataCID = 'QmR9nXy6XHmJcGLXHDjWN79BhaQEN21LU4kU4V2QvkdbDN';
   const metadataURI = `ipfs://${metadataCID}/${tokenId}.json`;
+  const metadataAccessURI = `https://ipfs.io/ipfs/${metadataCID}/${tokenId}.json`;
   const imageCID = 'QmesATCiAPVNA6X7C3TnbMnvZjK4g27HAV3cdr1aTKYeH4'
   const imageURI = `https://ipfs.io/ipfs/${imageCID}/${tokenId}.png`;
+
+  async function checkResource(url) {
+    try {
+      const response = await fetch(url, { method: 'HEAD' });
+      return response.ok;
+    } catch (error) {
+      console.error('Error checking file existence:', error);
+      return false;
+    }
+  }
+
+  const [nftExists, setNftExists] = useState(null);
+  useEffect(() => {
+    const checkNft = async () => {
+      const exists = await checkResource(metadataAccessURI);
+      setNftExists(exists);
+    };
+    checkNft();
+  }, [metadataAccessURI]);
 
   const [isMinted, setIsMinted] = useState(false);
   useEffect(() => {
@@ -71,9 +91,10 @@ function NFTImage({ tokenId, getCount }) {
     const uri = await contract.tokenURI(tokenId);
     alert(uri);
   }
-  return (
-    <div>
-      <img src={isMinted ? imageURI : "src/assets/placeholder.png"} width="240" height="240"></img>
+  if (nftExists) {
+    return (
+      <div>
+        <img src={isMinted ? imageURI : "src/assets/placeholder.png"} width="240" height="240"></img>
         <h5>ID #{tokenId}</h5>
         {!isMinted ? (
           <button onClick={mintToken}>
@@ -84,8 +105,12 @@ function NFTImage({ tokenId, getCount }) {
             Taken! Show URI
           </button>
         )}
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return;
+  }
+
 }
 
 export default Home;
